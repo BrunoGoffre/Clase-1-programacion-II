e233c364459ff2cb7d867fe850d2916a6112ea48
alumno <BrunoGoffredo22@gmail.com>
2019-09-04 21:45:27 -0300

    Clase 5

---
+++ ejercicio/Ejercicio Clase 5/Estante.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    class Estante
+    {
+        Producto[] productos;
+        int ubicacionEstante;
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];            
+        }
+        public Estante(int capacidad, int ubicacion) : this (capacidad)
+        {
+            this.ubicacionEstante = ubicacion;  
+        }
+        
+        public static string MostrarEstante(Estante x)
+        {
+            StringBuilder Str = new StringBuilder();
+            Str.AppendFormat($"En ubicacion {x.ubicacionEstante}\n");
+            foreach (Producto X in x.productos)
+            {
+                Str.AppendFormat($"{X}\n");                  
+            }
+            
+            return Str.ToString();
+        }
+
+        public static bool operator ==(Producto X, Estante Z)
+        {
+            bool retorno = false;
+            if (!(X is null) && !(Z is null))
+            {
+                for (int i = 0; i < Z.productos.Length; i++)
+                {
+                    if (X == Z.productos[i])
+                    {
+                        retorno = true;
+                    }
+                    else
+                    {
+                        retorno = false;
+                    }
+                }
+            }          
+            
+            return retorno;
+        }
+
+        public static bool operator !=(Producto x, Estante z)
+        {
+            return !(x == z);
+        }
+
+       public static bool operator +(Estante z, Producto x)
+        {
+            bool retorno = false;
+            if (!(z is null) && !(x is null))
+            {
+                if (x != z)
+                {
+                    for (int i = 0; i < z.productos.Length; i++)
+                    {
+                        if (z.productos[i] is null)
+                        {
+                            z.productos[i] = x;
+                            retorno = true;
+                        }
+                    }
+                }
+            }
+            return retorno;
+        }
+        public static Estante operator -(Estante z, Producto x)
+        {
+            Estante retorno = z;
+            if(!(z is null) && !(x is null))
+            {
+                if (x == z)
+                {
+                    for (int i = 0; i < z.productos.Length; i++)
+                    {
+                        if (z.productos[i] == x)
+                        {
+                            z.productos[i] = null;
+                            retorno = z;
+                        }
+                    }
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ ejercicio/Ejercicio Clase 5/Producto.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Clase_5
+{
+    public class Producto
+    {
+        string CodigoDeBarra;
+        string marca;
+        float precio;
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.marca = marca;
+            this.CodigoDeBarra = codigoDeBarra;
+            this.precio = precio;
+        }
+        
+        public static string MostrarProducto(Producto x)
+        {
+            string Str = $"{x.marca} Precio: {x.precio} su codigo de barra es: {x.CodigoDeBarra}";
+            return Str;
+        }
+
+        public static explicit operator string(Producto X)
+        {
+            return X.CodigoDeBarra;
+        }
+
+        public static bool operator ==(Producto A, Producto B)
+        {
+            if (!(A is null) && !(B is null))
+            {
+                if (A.marca == B.marca && A.CodigoDeBarra == B.CodigoDeBarra)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+
+        public static bool operator !=(Producto A, Producto B)
+        {
+            if (!(A == B))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator ==(Producto A, string Str)
+        {
+           if (A.marca == Str)
+            {
+                return true;
+            }
+           else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Producto A, string Str)
+        {
+            if (!(A.marca == Str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    
+
+    
+}
