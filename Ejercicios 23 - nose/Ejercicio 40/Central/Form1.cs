32c4a231ca86c1537ce20d2d495098a1770f350c
Bruno <BrunoGoffredo22@gmail.com>
2019-09-25 17:02:37 -0300

    Actualizacion

---
+++ Ejercicios 23 - nose/Ejercicio 40/Central/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CentralitaHerencia;
+
+namespace Central
+{
+    public partial class Form1 : Form
+    {
+        Centralita c = new Centralita("Bruno center");
+
+        public Form1()
+        {
+            InitializeComponent();             
+        }
+    }
+}
