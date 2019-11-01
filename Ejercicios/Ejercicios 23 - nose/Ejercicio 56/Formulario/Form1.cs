b667daf0c3ba14009ab3b499c00878f29f19fbf2
alumno <BrunoGoffredo22@gmail.com>
2019-10-31 22:11:08 -0300

    algo de ele tp y 56

---
+++ Ejercicios/Ejercicios 23 - nose/Ejercicio 56/Formulario/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Formulario
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        string text;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(dialog.OpenFile()))
+                {
+                    text = richTextBox1.Text = sr.ReadToEnd();
+                    path = dialog.FileName;
+                }
+            }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (text != richTextBox1.Text)
+            {
+                if (path != string.Empty)
+                {
+                    StreamWriter sw = new StreamWrite();
+                }
+            }
+        }
+    }
+}
