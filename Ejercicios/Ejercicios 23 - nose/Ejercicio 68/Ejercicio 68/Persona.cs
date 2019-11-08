124532c64d486e019f499ecac59de12c1c4b4bd2
alumno <BrunoGoffredo22@gmail.com>
2019-11-07 22:09:13 -0300

    eje 68

---
+++ Ejercicios/Ejercicios 23 - nose/Ejercicio 68/Ejercicio 68/Persona.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_68
+{
+    public delegate void DelegadoString();
+
+    public class Persona
+    {
+        string nombre;
+        string apellido;
+        DelegadoString EventoString;
+
+
+        public Persona()
+        {
+            this.nombre = string.Empty;
+            this.apellido = string.Empty;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+        public string Mostrar()
+        {
+            return $"Nombre: {this.nombre} Apellido {this.apellido}";
+        }
+    }
+}
